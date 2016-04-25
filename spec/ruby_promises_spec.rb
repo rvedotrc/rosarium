f9743453c52a25dc6e686461e54b4070d0ffb1de
Rachel Evans <git@rve.org.uk>
2016-04-26 00:24:36 +0100

    Refactor and test for .fulfill, .reject

--- spec/ruby_promises_spec.rb
+++ spec/ruby_promises_spec.rb
@@ -2,32 +2,59 @@ require "ruby_promises"
 
 describe MyConcurrent::Promise do
 
-  it "creates a pending promise" do
-    promise = MyConcurrent::Promise.new
+  def check_pending(promise)
     expect(promise.state).to eq(:pending)
     expect(promise).not_to be_fulfilled
     expect(promise).not_to be_rejected
-    # expect(promise.value).to eq(nil)
-    # expect(promise.reason).to eq(nil)
+    # expect(promise.value).to be_nil # should block
+    # expect(promise.reason).to be_nil # should block
   end
 
-  it "creates a fulfilled promise" do
-    promise = MyConcurrent::Promise.fulfill(7)
+  def check_fulfilled(promise, value)
     expect(promise.state).to eq(:fulfilled)
     expect(promise).to be_fulfilled
     expect(promise).not_to be_rejected
-    expect(promise.value).to eq(7)
-    expect(promise.reason).to eq(nil)
+    expect(promise.value).to eq(value)
+    expect(promise.reason).to be_nil
   end
 
-  it "creates a rejected promise" do
-    e = StandardError.new
-    promise = MyConcurrent::Promise.reject(e)
+  def check_rejected(promise, e)
     expect(promise.state).to eq(:rejected)
     expect(promise).not_to be_fulfilled
     expect(promise).to be_rejected
     expect(promise.value).to eq(nil)
     expect(promise.reason).to eq(e)
+  end
+
+  it "creates a pending promise" do
+    promise = MyConcurrent::Promise.new
+    check_pending promise
+  end
+
+  it "creates and then fulfills a promise" do
+    promise = MyConcurrent::Promise.new
+    check_pending promise
+    promise.fulfill 7
+    check_fulfilled promise, 7
+  end
+
+  it "creates and then rejects a promise" do
+    e = StandardError.new
+    promise = MyConcurrent::Promise.new
+    check_pending promise
+    promise.reject e
+    check_rejected promise, e
+  end
+
+  it "creates a fulfilled promise" do
+    promise = MyConcurrent::Promise.fulfill(7)
+    check_fulfilled promise, 7
+  end
+
+  it "creates a rejected promise" do
+    e = StandardError.new
+    promise = MyConcurrent::Promise.reject(e)
+    check_rejected promise, e
   end
 
 end
