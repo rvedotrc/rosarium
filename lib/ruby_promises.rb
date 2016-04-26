7b56dacb0732d7f42a77bc788e00a7f9d9e8090d
Rachel Evans <git@rve.org.uk>
2016-04-26 23:04:41 +0100

    Use a simple FixedThreadExecutor for .execute

--- lib/ruby_promises.rb
+++ lib/ruby_promises.rb
@@ -1,5 +1,65 @@
 module MyConcurrent
 
+  class FixedThreadExecutor
+
+    def initialize(max = 1)
+      @max = max
+      @mutex = Mutex.new
+      @waiting = []
+      @executing = 0
+      @threads = []
+    end
+
+    def submit(&block)
+      @mutex.synchronize do
+        @waiting << block
+        if @executing < @max
+          @executing = @executing + 1
+          t = Thread.new { execute_and_count_down }
+          @threads.push t
+        end
+      end
+    end
+
+    def discard
+      @mutex.synchronize { @waiting.clear }
+    end
+
+    def wait_until_idle
+      loop do
+        t = @mutex.synchronize { @threads.shift }
+        t or break
+        t.join
+      end
+    end
+
+    private
+
+    def execute_and_count_down
+      begin
+        execute
+      ensure
+        @mutex.synchronize do
+          @executing = @executing - 1
+        end
+      end
+    end
+
+    def execute
+      while true
+        block = @mutex.synchronize { @waiting.shift }
+        block or break
+        begin
+          block.call
+        rescue Exception => e
+        end
+      end
+    end
+
+  end
+
+  EXECUTOR = FixedThreadExecutor.new(10)
+
   class SimplePromise
 
     def self.new_deferred
@@ -140,7 +200,7 @@ module MyConcurrent
 
     def self.execute(&block)
       deferred = new_deferred
-      t = Thread.new do
+      EXECUTOR.submit do
         begin
           deferred.fulfill block.call
         rescue Exception => e
--- spec/ruby_promises_spec.rb
+++ spec/ruby_promises_spec.rb
@@ -1,5 +1,41 @@
 require "ruby_promises"
 
+describe MyConcurrent::FixedThreadExecutor do
+
+  it "runs a job" do
+    ex = MyConcurrent::FixedThreadExecutor.new(1)
+    done = false
+    ex.submit { done = true }
+    ex.wait_until_idle
+    expect(done).to be_truthy
+  end
+
+  it "discards exceptions" do
+    ex = MyConcurrent::FixedThreadExecutor.new(1)
+    done = false
+    ex.submit { raise "bang" }
+    ex.submit { done = true }
+    ex.wait_until_idle
+    expect(done).to be_truthy
+  end
+
+  it "runs jobs concurrently" do
+    ex = MyConcurrent::FixedThreadExecutor.new(3)
+    m = Mutex.new
+    done = []
+    3.times do
+      ex.submit do
+        m.synchronize { done << "s" }
+        sleep 0.1
+        m.synchronize { done << "e" }
+      end
+    end
+    ex.wait_until_idle
+    expect(done).to eq(%w[ s s s e e e ])
+  end
+
+end
+
 describe MyConcurrent::Promise do
 
   def check_pending(promise)
